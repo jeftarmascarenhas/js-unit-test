1dad4327ff2f793906caecbb56d8f27d7101d94d
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2019-06-22 10:48:36 -0300

    finished config and test coverage

--- src/main.js
+++ src/main.js
@@ -3,14 +3,7 @@ const FizzBuzz = (num) => {
   if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
   if (num % 3 === 0) return 'Fizz';
   if (num % 5 === 0) return 'Buzz';
-
   return num;
 };
-
-const teste = () => 'teste';
-const teste2 = () => 'teste2';
-
-teste();
-teste2();
 
 export default FizzBuzz;
