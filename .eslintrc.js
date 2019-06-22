f8140a8eaf99b0a46018747501d7cf07d9751475
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2019-06-21 22:04:09 -0300

    add rule in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module"
   },
-  rules: {}
+  rules: {
+    "no-unused-expressions": 0
+  }
 };
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -40,11 +40,17 @@ describe('Main', () => {
     arr.push(4);
     expect(arr).to.have.lengthOf(4);
   });
+
   it('should have a size of 2 when pop a value from the array', () => {
     arr.pop();
     // expect = espero; to = ligação; not = negação; include = condição
     expect(arr).to.not.include(3);
   });
+
+  it('should return true when pop 3 from the array', () => {
+    expect(arr.pop() === 3).to.be.true;
+  });
+
   it('should remove the value 3 when use pop in the array', () => {
     arr.pop();
     expect(arr).to.have.lengthOf(2);
